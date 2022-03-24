e21c8f66427fc244f83748bde48c5dd6d7501994
AaEzha <aaezha@gmail.com>
2022-03-25 05:08:21 +0700

    wip

--- app/Http/Controllers/MootaWebhookController.php
+++ app/Http/Controllers/MootaWebhookController.php
@@ -15,26 +15,23 @@ class MootaWebhookController extends Controller
      */
     public function __invoke(Request $request)
     {
-        MootaWebhook::create([
+        $data = MootaWebhook::create([
             'body' => $request->getContent()
         ]);
 
         $webhooks = [];
+        $response['id'] = $data->getKey();
+        $response['webhook_id'] = $data->body->id;
+        $response['bank_id'] = $data->body->bank_id;
+        $response['account_number'] = $data->body->account_number;
+        $response['bank_type'] = $data->body->bank_type;
+        $response['date'] = $data->body->date;
+        $response['amount'] = $data->body->amount;
+        $response['description'] = $data->body->description;
+        $response['type'] = $data->body->type;
+        $response['balance'] = $data->body->balance;
+        array_push($webhooks, $response);
 
-        foreach (MootaWebhook::latest()->get() as $data) {
-            $response['id'] = $data->getKey();
-            $response['webhook_id'] = $data->body->id;
-            $response['bank_id'] = $data->body->bank_id;
-            $response['account_number'] = $data->body->account_number;
-            $response['bank_type'] = $data->body->bank_type;
-            $response['date'] = $data->body->date;
-            $response['amount'] = $data->body->amount;
-            $response['description'] = $data->body->description;
-            $response['type'] = $data->body->type;
-            $response['balance'] = $data->body->balance;
-
-            array_push($webhooks, $response);
-        }
         return response()->json([
             'status' => 200,
             'message' => 'Success',
