47423c162e86f912fa620d8677e229f98da2fb47
AaEzha <aaezha@gmail.com>
2022-03-25 05:06:44 +0700

    wip

--- app/Http/Controllers/MootaWebhookController.php
+++ app/Http/Controllers/MootaWebhookController.php
@@ -18,10 +18,27 @@ class MootaWebhookController extends Controller
         MootaWebhook::create([
             'body' => $request->getContent()
         ]);
+
+        $webhooks = [];
+
+        foreach (MootaWebhook::latest()->get() as $data) {
+            $response['id'] = $data->getKey();
+            $response['webhook_id'] = $data->body->id;
+            $response['bank_id'] = $data->body->bank_id;
+            $response['account_number'] = $data->body->account_number;
+            $response['bank_type'] = $data->body->bank_type;
+            $response['date'] = $data->body->date;
+            $response['amount'] = $data->body->amount;
+            $response['description'] = $data->body->description;
+            $response['type'] = $data->body->type;
+            $response['balance'] = $data->body->balance;
+
+            array_push($webhooks, $response);
+        }
         return response()->json([
             'status' => 200,
             'message' => 'Success',
-            'data' => $request->getContent()
+            'data' => $webhooks
         ]);
     }
 }
